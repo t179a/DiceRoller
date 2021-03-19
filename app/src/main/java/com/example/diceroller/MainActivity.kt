a3b1982e9adae7fada65b14c2949c9f81daf861a
t179a <azmtzk179@gmail.com>
2021-03-20 00:03:27 +0900

    change the text. display a string. display a random number.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.diceroller.R.id.roll_button
 
@@ -17,7 +18,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice(){
-        Toast.makeText(this, "button clicked",
-            Toast.LENGTH_SHORT).show()
+        /*Toast.makeText(this, "button clicked",
+            Toast.LENGTH_SHORT).show()*/
+        val randomInt = (1..6).random()
+
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
     }
 }
